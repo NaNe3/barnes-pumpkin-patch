46366c4ed04755b1fffeeb03f15409e3f2abcb2a
NaNe3 <elithedev@gmail.com>
2023-11-06 18:07:47 -0700

    I finished the navigation bar lol

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,33 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const navBar = () => {
+
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      document.getElementById("logo").style.height = "60px";
+    } else {
+      document.getElementById("logo").style.height = "120px";
+    }
+
+    return '';
+  };
+
+  if (window.innerWidth > 768) {
+    window.addEventListener('scroll', navBar)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id='navbar' className="navbar">
+          <img id='logo' className='logo' src='./src/logo.png'></img>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='body-container'></div>
     </>
   )
 }
+
+// NEXT STEP:   CREATE COMPONENT FOR THE SLIDESHOW!!!!!!!
 
 export default App
